1878ad652fec8dd6b6eb518ea9a0bcf03070b86b
Bruno <bfsilva98@gmail.com>
2023-06-15 11:43:29 -0300

    FINISHED PURE FUNC

--- Course 2/Functional programming/pure_function.py
+++ Course 2/Functional programming/pure_function.py
@@ -1,10 +1,13 @@
-#THIS IS AN EXAMPLE OF TRADITIONAL FUNCTION
+#THIS IS AN EXAMPLE OF *PURE* FUNCTION
 
 my_list = [1, 2, 3]
 
-def add_to_list(item):
-    return my_list.append(item)
+def add_to_list(lst, item):
+    nl = lst.copy()
+    nl.append(item)
+    return nl
 
-add_to_list(4)
+new_list = add_to_list(my_list, 4)
 
 print(my_list)
+print(new_list)
